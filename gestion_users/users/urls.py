f2265e3e13bcac4a04b9a1ba871e0712c4748153
masango2001 <masangoblaise5@gmail.com>
2025-03-20 08:19:01 +0200

    nouvelles modifications sur operation CRUD

--- gestion_users/users/urls.py
+++ gestion_users/users/urls.py
@@ -3,4 +3,11 @@ from .import views
 urlpatterns=[
     path('',views.home, name='home'),
     path('service/',views.service, name='service'),
+        
+    path('create/',views.create_student, name='create_student'),
+    
+    path('edit/<int:id>/',views.edit_student, name='edit_student'),
+   path('delete/<int:id>/',views.delete_student, name='delete_student'),
+    
+   
 ]
--- gestion_users/users/views.py
+++ gestion_users/users/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect,get_object_or_404
+from . models import Student
+from . form import studentForm
 def home(request):
-    return render(request,'index.html')
+    students=Student.objects.all()
+    return render(request,'index.html',{'students':students})
 def service(request):
     return render(request,'service.html')
+def create_student(request):
+    if request.method=='POST':
+        form=studentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form=studentForm()
+    return render(request,'form.html',{'form':form})
+def edit_student(request, id):
+    student = Student.objects.get(id=id)
+    if request.method == 'POST':
+        form = studentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()  
+            return redirect('home')
+    else:
+        form = studentForm(instance=student) 
+
+    return render(request, 'form.html', {'form': form})
+   
+def delete_student(request,id):
+    student=get_object_or_404(Student,id=id)
+    if request.method=='POST':
+        student.delete()
+        return redirect('home')
+    return render(request,'index.html',{'student':student}) 
