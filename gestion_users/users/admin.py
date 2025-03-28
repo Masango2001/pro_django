5571b4f63e09ad409678e5ba29e651c13f3a2af5
masango2001 <masangoblaise5@gmail.com>
2025-03-28 18:21:06 +0200

    adding new in file admin

--- gestion_users/users/admin.py
+++ gestion_users/users/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from . models import Student
-admin.site.register(Student)
 
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'email', 'age')  
+    search_fields = ('name', 'surname', 'email')  
+
+    
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+    
+        return queryset
--- gestion_users/users/form.py
+++ gestion_users/users/form.py
@@ -1,6 +1,13 @@
 from django import forms
 from .models import Student
+from django.forms import TextInput, EmailInput, NumberInput
 class studentForm(forms.ModelForm):
     class Meta:
         model=Student
         fields=['name','surname','email','age']  
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Nom'}),
+            'surname': TextInput(attrs={'class': 'form-control', 'placeholder': 'Prénom'}),
+            'email': EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+            'age': NumberInput(attrs={'class': 'form-control', 'placeholder': 'Âge'}),
+        }
